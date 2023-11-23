fe3995f480c90ee44a007b81a9b912f1bb0fc113
Alexsander Telles <alevtelles@gmail.com>
2023-11-23 11:37:34 -0300

    Update

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -9,21 +9,19 @@ const MarketingPage = () => {
           <Medal className="h-6 w-6 mr-2" />
           Nº 1 GERENCIAMENTO DE TAREFAS
         </div>
-        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6">
-          Ajuda a sua equipe se mover
+        <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 font-bold max-w-xs md:max-w-4xl">
+          O ODS reúne as tarefas, colegas de equipe e ferramentas
         </h1>
         <div className="text-3xl md:text-6xl bg-gradient-to-r from-fuchsia-600 to-pink-600 text-white px-4 p-2 rounded-md b-4 w-fit">
-          Trabalhar em frente
+          em um único lugar
         </div>
       </div>
 
       <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
-        Colabore e gerencie seus projetos, Para pequenas, médias e grandes
-        empresas, a forma como sua equipe trabalha é única, faça tudo isso com o
-        ODS Gerenciador de Projetos.
+        Mantenha tudo em um só lugar, mesmo se o time for distribuído.
       </div>
       <div>
-        <Button>Teste</Button>
+        <Button>Obter Grátis</Button>
       </div>
     </div>
   );
