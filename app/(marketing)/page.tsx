7e82d695f74438548423bdbdbe8730dd5911721d
Alexsander Telles <alevtelles@gmail.com>
2023-11-23 21:59:48 -0300

    update code

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -24,8 +24,7 @@ const MarketingPage = () => {
         )}
       >
         <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
-          <Medal className="h-6 w-6 mr-2" />
-          Nº 1 GERENCIAMENTO DE TAREFAS
+          <Medal className="h-6 w-6 mr-2" />O Nº 1 PARA GERENCIAR TAREFAS
         </div>
         <h1 className="text-3xl md:text-6xl text-center text-neutral-800 mb-6 font-bold max-w-xs md:max-w-4xl">
           Todas as suas tarefas, times de desenvolvimento e muitas ferramentas,
